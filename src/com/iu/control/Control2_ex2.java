d08a15d3fb7c25df2cefec3bc3e233526a6c9975
parkvsmin <sdfjkl658@naver.com>
2022-07-06 22:53:36 +0900

    어려워

--- src/com/iu/control/Control2_ex2.java
+++ src/com/iu/control/Control2_ex2.java
@@ -11,23 +11,24 @@ public class Control2_ex2 {
 		char ch = ' '; // => b
 		
 		Scanner sc = new Scanner (System.in);
-		System.out.println("1-5 사이의 숫자 입력");
 		num = sc.nextInt(); // 1-5;
+		System.out.println("1-5 사이의 숫자 입력");
 		
-		
+		ch = sc.nextLine().charAt(0);
 		System.out.println("알파벳을 입력");
-		ch = sc.next().charAt(0);
 		
 		ch = (char)(ch+num);
 		System.out.println("ch : " + ch);
-		// num :1 ch1 :a 출력 b
-		// num :2 ch1 :a 출력 c
-		// num :1 ch1 :z 출력 a
-		// num :2 ch1 :z 출력 b
+		// num :1 ch :a 출력 b
+		// num :2 ch :a 출력 c
+		// num :1 ch :z 출력 a
+		// num :2 ch :z 출력 b
 		
 		System.out.println("알파벳을 출력");
 		
-		if()
+		if((ch>=97) & (ch<=122)); {
+			System.out.println("소문자");
+		}
 		
 		
 		
