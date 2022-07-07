9c445d9a9a0f776d1c19ad3aa7dfc0f92cde6695
parkvsmin <sdfjkl658@naver.com>
2022-07-07 12:11:11 +0900

    11

--- src/com/iu/control/Control2_ex2.java
+++ src/com/iu/control/Control2_ex2.java
@@ -11,37 +11,38 @@ public class Control2_ex2 {
 		char ch = ' '; // => b
 		
 		Scanner sc = new Scanner (System.in);
-		num = sc.nextInt(); // 1-5;
 		System.out.println("1-5 사이의 숫자 입력");
+		num = sc.nextInt(); // 1-5;
 		
-		ch = sc.nextLine().charAt(0);
 		System.out.println("알파벳을 입력");
+		ch = sc.next().charAt(0);
 		
-		ch = (char)(ch+num);
+		int result = ch + num;
+		
+		//소문자가 범위를 벗어났을 때
+		if(result>'z') {
+			result = result - 'z'-1; //2
+			result = 'a'+result;
+			
+		}
+		//대문자가 범위를 벗어났을 떄
+		if(result>'Z' && result<'a') {
+			result = result-'Z'-1;
+			result = 'A'+result;
+			System.out.println("대문자 영역");
+		}
+		ch = (char)result;
 		System.out.println("ch : " + ch);
 		// num :1 ch :a 출력 b
 		// num :2 ch :a 출력 c
 		// num :1 ch :z 출력 a
 		// num :2 ch :z 출력 b
 		
-		System.out.println("알파벳을 출력");
-		
-		if((ch>=97) & (ch<=122)); {
-			System.out.println("소문자");
-		}
-		
-		//Study Edition
-		//Home Edition
 
-		
-		
-		
-		
-		
-		
 		
 		
 
 	}
-
+	
 }
+
